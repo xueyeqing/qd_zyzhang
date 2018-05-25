125232e5b189f2055524d787f7ee848aab97144a
zhangzhengyi <zhangtaihu101@163.com>
2018-05-25 09:58:41 +0800

    按规律分配优化

--- html/demo/test/test.js
+++ html/demo/test/test.js
@@ -1,17 +1,38 @@
 /**
  * Created by a1 on 18/5/24.
  */
-let a = [11,22,33,44,55,66,77];
-let b =[2,1,2,3,4,2];
-let c =[];
-let index = 0;
 
-for(let i=0;i<b.length;i++){
-    c = a.slice(index,index+b[i]);
-    if(index==a.length){
-        index=0;
+ /**
+  * 分配老师1（按规律分配）
+  */
+let teachers = [11,22,33,44,55,66,77]; //老师
+let qindex =[2,1,2,3,4,2]; //题数 
+let len = teachers.length; // 老师的个数
+let start = 0; // 索引
+for(let i=0;i<qindex.length;i++){ // 循环题目
+    let data = [];
+    for(let j=start;j<start+qindex[i];j++){ 
+       var ind = j % len ; // 取余
+       data.push(teachers[ind]);
     }
-    if(index>a.length){}
-    index +=b[i];
-    console.log(c);
+    start = ind+1;
+    console.log(data);
 }
+console.log('---------------------------------------')
+/**
+  * 分配老师2（按规律分配）
+  */
+var index = 0;
+for (let i = 0; i < qindex.length; i++) {
+    let t1 = [];
+    for (let j = 0; j < qindex[i]; j++) {
+        if(index == len) {
+            index =0;
+            t1.push(teachers[index]);
+        }else{
+            t1.push(teachers[index]);
+        }
+        index++;
+    }
+    console.log(t1);
+} 
